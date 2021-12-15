1b356bf93261e28b2ee43b07be232299b1439982
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2021-12-15 12:37:21 +0900

    solve : BOJ-2292 벌집

---
+++ BOJ/2292.java
@@ -0,0 +1,41 @@
+/*
+ 	BOJ 2292번 벌집
+
+	문제
+    위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다.
+    숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오.
+    예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
+*/
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 1 + 6 = 7
+ * 7 + 12 = 19
+ * 19 + 18 = 37
+ * 37 + 24 = 61
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 1;
+
+        if(n > 1) { // n이 1보다 클 때
+            for(int i = 1; i < n; i++) {
+                count += (6 * i); // 6의 배수만큼 계속해서 더해줌
+                if(n <= count) {
+                    System.out.println(i + 1); // 시작점도 함께 넣어줘야 하니 + 1
+                    return;
+                }
+            }
+        }
+        System.out.println(1);
+    }
+}
