b90c51d83b97f278314ddd62609428a9e3e68734
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-04-10 20:08:48 +0900

    solve : PRG-LV1 문자열을 정수로 바꾸기

---
+++ Programmers/문자열을 정수로 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        // 테스트 코드
+        System.out.println(solution("-1234"));
+    }
+
+    public static int solution(String s) {
+
+        return Integer.parseInt(s);
+    }
+}
