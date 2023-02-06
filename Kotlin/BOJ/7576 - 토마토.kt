a2e9fde553f6517b9407f0542fdcb0380b0ad501
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2023-02-06 21:38:22 +0900

    solve : BOJ-7576 토마토

--- Kotlin/BOJ/7576 - 토마토.kt
+++ Kotlin/BOJ/7576 - 토마토.kt
@@ -1,74 +1,61 @@
 import java.util.*
 
-lateinit var g: Array<Array<IntArray>>
-lateinit var queue: Queue<Triple<Int, Int, Int>>
+lateinit var array: Array<IntArray>
 
-val dx = intArrayOf(-1, 1, 0, 0, 0, 0)
-val dy = intArrayOf(0, 0, -1, 1, 0, 0)
-val dz = intArrayOf(0, 0, 0, 0, -1, 1)
+val dx = intArrayOf(-1, 1, 0, 0)
+val dy = intArrayOf(0, 0, -1, 1)
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val (m, n, h) = readLine().split(" ").map { it.toInt() }
+    val (m, n) = readLine().split(" ").map { it.toInt() }
 
-    g = Array(h) { Array(n) { IntArray(m) } }
-    queue = LinkedList()
+    array = Array(n) { IntArray(m) }
 
-    var noRipe = 0
+    repeat(n) { i ->
+        array[i] = readLine().split(" ").map { it.toInt() }.toIntArray()
+    }
 
-    repeat(h) { i ->
-        repeat(n) { j ->
-            val s = readLine().split(" ").map { it.toInt() }
-            repeat(m) { k ->
-                when (s[k]) {
-                    1 -> queue.add(Triple(i, j, k))
-                    0 -> noRipe++
-                }
-                g[i][j][k] = s[k]
+    val level = BFS()
+
+    repeat(array.size) { i ->
+        repeat(array[0].size) { j ->
+            if(array[i][j] == 0) {
+                println(-1)
+                return@with
             }
         }
     }
 
-    if (noRipe == 0) {
-        println(0)
-        return@with
-    }
-
-    val res = bfs(n, m, h, noRipe)
-    println(res)
+    println(level)
 }
 
-private fun bfs(n: Int, m: Int, h: Int, tomato: Int): Int {
-    var cnt = 0
-    var ripeTomato = 0
+fun BFS(): Int {
+    val queue = LinkedList<Pair<Int, Int>>()
 
-    while (queue.isNotEmpty()) {
-        val size = queue.size
+    repeat(array.size) { i ->
+        repeat(array[0].size) { j ->
+            if(array[i][j] == 1) queue.add(i to j)
+        }
+    }
 
-        for (a in 0 until size) {
-            val node = queue.poll()
-            val x = node.second
-            val y = node.third
-            val z = node.first
+    var level = 0
 
-            for (i in 0 until 6) {
-                val nx = x + dx[i]
-                val ny = y + dy[i]
-                val nz = z + dz[i]
+    while(queue.isNotEmpty()) {
+        repeat(queue.size) {
+            val curr = queue.poll()
 
-                if (nx in 0 until n && ny in 0 until m && nz in 0 until h) {
-                    if (g[nz][nx][ny] == 0) {
-                        g[nz][nx][ny] = 1
-                        queue.add(Triple(nz, nx, ny))
-                        ripeTomato++
-                    }
-                }
-                if (ripeTomato == tomato) {
-                    cnt++
-                    return cnt
-                }
+            for(i in 0 until 4) {
+                val x = curr.first + dx[i]
+                val y = curr.second + dy[i]
+
+                if(x !in array.indices || y !in array[0].indices) continue
+                if(array[x][y] != 0) continue
+
+                array[x][y] = 1
+                queue.add(x to y)
             }
         }
-        cnt++
+        level++
     }
-    return -1
+
+    return level - 1
 }
