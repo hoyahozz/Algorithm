ebe6766d2b6aba1bc91dbe3b0c5a691baa9fe406
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2023-03-09 23:34:29 +0900

    solve : BOJ-2579 계단 오르기

---
+++ Kotlin/BOJ/2579 - 계단 오르기.kt
@@ -0,0 +1,23 @@
+import kotlin.math.max
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val stairs = IntArray(n + 1)
+    val dp = IntArray(n + 1)
+
+    repeat(n) {
+        stairs[it + 1] = readLine().toInt()
+    }
+
+    dp.apply {
+        this[1] = stairs[1]
+        if (n >= 2) this[2] = stairs[1] + stairs[2]
+    }
+
+    for (i in 3 until dp.size) {
+        dp[i] = stairs[i] + max(dp[i - 3] + stairs[i - 1], (dp[i - 2]))
+    }
+
+    println(dp.last())
+}
+
